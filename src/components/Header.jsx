999d3a7a908bf97773ec6f02b93ebbeedd26e266
Ocean Hillman <oceanhillman@gmail.com>
2023-09-19 14:28:17 -0400

    Add Tailwind inline classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,37 +7,35 @@ import Logo from "../logo.png";
 
 function Header() {
     return (
-        <Navbar collapseOnSelect expand="lg" className="d-flex align-items-center justify-content-center text-center">
+        <Navbar collapseOnSelect expand="lg" className="d-flex align-items-center justify-content-center text-center bg-transparent">
         <Container style={{display: "flex"}}  className="d-flex justify-content-center">
         <Row>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="/about">About</Nav.Link>
-                <NavDropdown title="Resources" id="collapsible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
+                <Nav.Link className="font-nav text-white" href="/about">About</Nav.Link>
+                <NavDropdown className="font-nav" title="Resources" id="collapsible-nav-dropdown">
+                    <NavDropdown.Item className="font-nav text-white" href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item className="font-nav text-white" href="#action/3.2">
                         Another action
                     </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Item className="font-nav text-white" href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
+                    <NavDropdown.Item className="font-nav text-white" href="#action/3.4">
                         About Online Poker
                     </NavDropdown.Item>
                 </NavDropdown>
-                {/* <Col className="d-flex align-items-center">
-                    <Row className="flex-nowrap"> */}
-                        <Navbar.Brand href="/">
-                            <div id="logo-wrapper">
-                                <img src={Logo} id="logo" alt="Logo"></img>
-                            </div>
-                            <div id="title-wrapper">
-                                <p id="brand-name">BigBadBabar</p>
-                                <p id="brand-slogan">Online Poker Resources</p>
-                            </div>
-                        </Navbar.Brand>
-                <Nav.Link href="#pricing">Good Stuff</Nav.Link>
-                <Nav.Link href="#pricing">Contact</Nav.Link>
+                <Navbar.Brand className="flex items-center" href="/">
+                    <div>
+                        <img className="w-20 h-20" src={Logo} id="logo" alt="Logo"></img>
+                    </div>
+                    <div>
+                        <p id="brand-name" className="font-brand text-white">BigBadBabar</p>
+                        <p id="brand-slogan" className="font-slogan text-white">Online Poker Resources</p>
+                    </div>
+                </Navbar.Brand>
+                <Nav.Link className="font-nav text-white" href="#pricing">Good Stuff</Nav.Link>
+                <Nav.Link className="font-nav text-white" href="#pricing">Contact</Nav.Link>
             </Nav>
             </Navbar.Collapse>
             </Row>
