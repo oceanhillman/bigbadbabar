00311d0d2c42f923558c2ffa924ca7f93494c95d
Ocean Hillman <oceanhillman@gmail.com>
2023-09-21 12:19:22 -0400

    Change text style

--- src/components/PageBanner.jsx
+++ src/components/PageBanner.jsx
@@ -1,15 +1,15 @@
 
 function PageBanner(props) {
     const body = props.content?.map(paragraph => 
-        <p className="font-body font-bold text-xl">
+        <p className="font-body text-xl">
             {paragraph}
         </p>
     );
 
     return(
-        <div className="container mx-auto my-5 py-6 text-white">
+        <div className="container mx-auto my-5 py-6 text-dark">
             <div class="columns-lg" className="mx-40 text-center">
-                <h1 className="pb-3 text-center font-title font-bold">
+                <h1 className="pb-3 font-title font-bold">
                     {props.title}
                 </h1>
                 {body}
