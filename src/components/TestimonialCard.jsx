6935fb3ed31f32465cb838462fe5f5f7a33d35a2
Ocean Hillman <oceanhillman@gmail.com>
2023-09-22 15:31:14 -0400

    Adjust spacing and background

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,18 +1,18 @@
 
 function TestimonialCard(props) {
     return (
-        <div className="flex-column justify-between min-h-full flex border-1 px-4 bg-cyan-200/20 border-dark shadow-md">
-            <div className="w-32 h-32 my-4 mx-auto">
-                <img src={props.imgURL} className="content-start w-32 h-32 mx-auto border-2 rounded-full border-dark" alt="Testimonial profile" />
+        <div className="flex-column justify-between min-h-full flex border-1 px-4 bg-cyan-100/50 border-dark shadow-md">
+            <div className="w-32 h-32 mt-5 mb-4 mx-auto">
+                <img src={props.imgURL} className="content-start w-32 h-32 mx-auto border-2 bg-almost-white rounded-full border-dark" alt="Testimonial profile" />
             </div>
-            <p className="h-64 mt-4 content-center font-body text-justify">
+            <p className="h-80 mt-2 text-lg content-center font-nav text-center">
                 "{props.statement}"
             </p>
             <div>
-                <p className="mb-0 font-body font-bold text-center ">
+                <p className="mb-0 text-lg font-body font-bold text-center ">
                     - {props.name}
                 </p>
-                <p className="mt-0 font-body text-sm text-center italic">
+                <p className="mb-2 font-body text-md text-center italic">
                     {props.about}
                 </p>
             </div>
