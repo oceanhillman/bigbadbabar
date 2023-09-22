350e0f3491625d72035a269fcd838867a4e595d9
Ocean Hillman <oceanhillman@gmail.com>
2023-09-22 12:40:16 -0400

    Add padding

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,7 +1,7 @@
 
 function NavButton(props) {
     return (
-        <a href={props.url} className="mx-10 font-nav font-extrabold no-underline text-xl text-almost-white hover:text-cloudy">
+        <a href={props.url} className="mx-10 font-nav font-extrabold no-underline text-xl p-2 text-almost-white hover:text-cloudy">
             {props.name}
         </a>
     )
