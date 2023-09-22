bddcd23a2123bf5498ec574b643f1e408372e41a
Ocean Hillman <oceanhillman@gmail.com>
2023-09-22 12:41:03 -0400

    Fix internal alignment

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,22 +1,20 @@
 
 function TestimonialCard(props) {
     return (
-        <div className="flex-column flex justify-between border-1 px-4 border-dark bg-dark/5">
+        <div className="flex-column justify-between min-h-full flex border-1 px-4 bg-cyan-200/20 border-dark shadow-md">
             <div className="w-32 h-32 my-4 mx-auto">
                 <img src={props.imgURL} className="content-start w-32 h-32 mx-auto border-2 rounded-full border-dark" alt="Testimonial profile" />
             </div>
-            <div className="flex-2  ">
-                <p className="content-center font-body text-justify">
-                    "{props.statement}"
+            <p className="h-64 mt-4 content-center font-body text-justify">
+                "{props.statement}"
+            </p>
+            <div>
+                <p className="mb-0 font-body font-bold text-center ">
+                    - {props.name}
                 </p>
-                <div className="flex-1">
-                    <p className="mb-0 font-body font-bold text-center ">
-                        - {props.name}
-                    </p>
-                    <p className="mt-0 font-body text-sm text-center italic">
-                        {props.about}
-                    </p>
-                </div>
+                <p className="mt-0 font-body text-sm text-center italic">
+                    {props.about}
+                </p>
             </div>
         </div>
     )
