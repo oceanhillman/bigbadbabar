fe29f170f2e67c5ac81830c992fae2e435c92014
Ocean Hillman <oceanhillman@gmail.com>
2023-09-22 15:28:42 -0400

    Replace a tag with NavLink and use dark theme

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,16 +1,17 @@
+import { NavLink } from "react-router-dom";
 import Logo from "../images/logo.png";
 
 function Brand() {
     return (
-        <a href="/" className="flex mx-4 items-center no-underline">
+        <NavLink to="/" className="flex mx-4 items-center no-underline">
             <div className="w-20 h-20">
                 <img className="w-20 h-20" src={Logo} id="logo" alt="Logo"></img>
             </div>
             <div>
                 <p className="font-brand my-0 text-3xl tracking-wider text-dark">BigBadBabar</p>
-                <p className="font-slogan -mt-2 mb-0 font-xl font-extrabold text-white">Online Poker Resources</p>
+                <p className="font-slogan -mt-2 mb-0 font-xl font-extrabold text-dark">Online Poker Resources</p>
             </div>
-        </a>
+        </NavLink>
     )
 }
 
