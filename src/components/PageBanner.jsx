cb68ccd11802d1d55057148b8319c8409b821f2c
Ocean Hillman <oceanhillman@gmail.com>
2023-09-22 12:40:41 -0400

    Adjust margin and background shadow

--- src/components/PageBanner.jsx
+++ src/components/PageBanner.jsx
@@ -7,7 +7,7 @@ function PageBanner(props) {
     );
 
     return(
-        <div className="my-5 py-5 text-almost-white bg-dark bg-opacity-50">
+        <div className="mt-8 py-5 text-white border-y border-dark/10 bg-gradient-to-r from-dark/10 from-10% via-dark/30 to-dark/10 to-90%">
             <div className="text-center">
                 <h1 className="pb-3 font-title font-bold">
                     {props.title}
