892034735bcd58acee1d2b9beba3eac6ea87e8f8
Ocean Hillman <oceanhillman@gmail.com>
2023-09-22 15:29:54 -0400

    Switch a tags for NavLink

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,9 +1,10 @@
+import { NavLink } from "react-router-dom";
 
 function NavButton(props) {
     return (
-        <a href={props.url} className="mx-10 font-nav font-extrabold no-underline text-xl p-2 text-almost-white hover:text-cloudy">
+        <NavLink to={props.url} className="mx-10 font-nav font-extrabold no-underline text-xl p-2 text-dark hover:text-almost-white">
             {props.name}
-        </a>
+        </NavLink>
     )
 }
 
