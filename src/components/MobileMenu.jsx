a71221f512610505ae4e1a2f35f7d931bfe4d64b
Ocean Hillman <oceanhillman@gmail.com>
2023-09-26 15:55:10 -0400

    Create component

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,9 @@
+import hamburgerMenu from "../images/hamburger-menu.svg";
+
+function MobileMenu() {
+    return(
+        <img src={hamburgerMenu} className="ml-4 mr-auto justify-self-start" />
+    )
+}
+
+export default MobileMenu;
