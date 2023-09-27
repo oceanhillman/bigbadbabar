4ce4125e142b3cd45253368415ae0a9f7234f98e
Ocean Hillman <oceanhillman@gmail.com>
2023-09-27 14:15:43 -0400

    Add size conditional

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,7 +4,8 @@ function InputField(props) {
         <input 
             type={props.type} 
             placeholder={props.placeholder}
-            className="h-10 w-full my-1 pt-3 bg-transparent border-b border-dark placeholder-dark text-dark font-nav focus:outline-none"
+            className={`h-10 my-1 pt-3 bg-transparent border-b border-dark placeholder-dark text-dark font-nav focus:outline-none 
+                ${props.size==="small" ? "col-span-1" : "col-span-2"}`}
         />
     )
 }
