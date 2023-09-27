c7c46203ae9b1a74809fe8149e31bd9e9649a414
Ocean Hillman <oceanhillman@gmail.com>
2023-09-27 14:16:02 -0400

    Delete and integrate in Header.jsx

--- src/components/MobileMenu.jsx
+++
@@ -1,9 +0,0 @@
-import hamburgerMenu from "../images/hamburger-menu.svg";
-
-function MobileMenu() {
-    return(
-        <img src={hamburgerMenu} className="ml-4 mr-auto justify-self-start" />
-    )
-}
-
-export default MobileMenu;
