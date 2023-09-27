014be0f296ea681e73facf17cfa51f8607640344
Ocean Hillman <oceanhillman@gmail.com>
2023-09-27 14:15:25 -0400

    Add padding

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -3,7 +3,7 @@ function InputBox(props) {
     return (
         <textarea 
             placeholder={props.placeholder}
-            className="h-28 w-full bg-dark/10 my-4 border border-dark placeholder-dark text-dark font-nav focus:outline-none"
+            className="h-28 w-full bg-dark/10 my-4 p-1 border border-dark placeholder-dark text-dark font-nav focus:outline-none"
         />
     )
 }
