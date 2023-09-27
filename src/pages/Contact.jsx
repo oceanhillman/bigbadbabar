44cd787fdc34816a6a3273f4157359b232561c1b
Ocean Hillman <oceanhillman@gmail.com>
2023-09-27 14:17:34 -0400

    Simplify to universal responsive design

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,70 +7,19 @@ import SocialButton from "../components/SocialButton";
 import telegramIcon from "../images/telegram-icon.svg";
 import xIcon from "../images/x-icon.svg";
 import skypeIcon from "../images/skype-icon.svg";
+import ActionBanner from "../components/ActionBanner";
 
 function Contact() {
     return (
-        <React.Fragment>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <div className="min-h-screen">
-                <div className="grid gap-x-20 lg:mx-16 xl:mx-64 xl:my-24 xl:grid-cols-2">
-                    <div className="col-span-1">
-                        <div className="hidden xl:block">
-                            <h1 className="font-title font-bold text-5xl text-dark tracking-wide text-center xl:text-left">Get In Contact</h1>
-                            <p className="my-5 font-body text-2xl">
-                                Feel free to contact me anytime by email or by using the social links below and I'll get back to you as soon as possible.
-                            </p>
-                        </div>
-                        <div className="hidden xl:flex flex-col justify-center">
-                            <div className="flex flex-row justify-center">
-                                <SocialButton
-                                    type="Telegram"
-                                    url="https://t.me/bigbadbabar"
-                                    icon={telegramIcon}
-                                />
-                                <SocialButton
-                                    type="X"
-                                    url="https://twitter.com/realbigbadbabar"
-                                    icon={xIcon}
-                                />
-                                <SocialButton
-                                    type="Skype"
-                                    url="https://join.skype.com/invite/JWGmIdOskRQS"
-                                    icon={skypeIcon}
-                                />
-                            </div>
-                            <Link to="https://forumserver.twoplustwo.com/members/44298/" className="my-3 text-center text-lg font-body text-dark">
-                                Two Plus Two Poker Forums
-                            </Link>
-                        </div>
-                    </div>
+            <div className="flex-1">
+                <div className="flex mx-auto sm:my-2 md:my-3 lg:my-4 xl:my-5 max-w-xl">
                     <ContactForm />
                 </div>
-                <div className="xl:hidden flex flex-col justify-center">
-                    <div className="flex flex-row justify-center">
-                        <SocialButton
-                            type="Telegram"
-                            url="https://t.me/bigbadbabar"
-                            icon={telegramIcon}
-                        />
-                        <SocialButton
-                            type="X"
-                            url="https://twitter.com/realbigbadbabar"
-                            icon={xIcon}
-                        />
-                        <SocialButton
-                            type="Skype"
-                            url="https://join.skype.com/invite/JWGmIdOskRQS"
-                            icon={skypeIcon}
-                        />
-                    </div>
-                    <Link to="https://forumserver.twoplustwo.com/members/44298/" className="my-3 text-center text-lg font-body text-dark">
-                        Two Plus Two Poker Forums
-                    </Link>
-                    </div>
             </div>
             <Footer />
-        </React.Fragment>
+        </div>
     )
 }
 
