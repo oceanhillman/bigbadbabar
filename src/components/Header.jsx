2998df1786e43be223f925f0a22abdab302f7dcf
Ocean Hillman <oceanhillman@gmail.com>
2023-09-27 14:15:06 -0400

    Add mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import NavButton from "./NavButton";
 import Brand from "./Brand";
-import MobileMenu from "./MobileMenu";
+import MobileNavButton from "./MobileNavButton";
+import hamburgerMenu from "../images/hamburger-menu.svg";
 
 
 function Header() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function MobileMenu() {
+
+        function handleClick() {
+            setIsOpen(!isOpen);
+        }
+    
+        return(
+             <img src={hamburgerMenu} className="ml-4 mr-auto justify-self-start" onClick={handleClick}/>
+        )
+    }
+
     return (
         <header>
             {/* Large Header */}
@@ -31,6 +48,26 @@ function Header() {
                 <MobileMenu />
                 <Brand />
             </nav>
+            <div className={`sm:hidden ${isOpen ? "block" : "hidden"}`}>
+                <div className="flex flex-col">
+                    <MobileNavButton 
+                        title="About"
+                        url="/about"
+                    />
+                   <MobileNavButton 
+                        title="Resources"
+                        url="/resources"
+                    />
+                    <MobileNavButton 
+                        title="Contact"
+                        url="/contact"
+                    />
+                    <MobileNavButton 
+                        title="Good Stuff"
+                        url="/good-stuff"
+                    />
+                </div>
+            </div>
         </header>
     )
 }
