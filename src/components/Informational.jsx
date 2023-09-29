1f363be0cae354ec69255321df88f0f95ab70237
Ocean Hillman <oceanhillman@gmail.com>
2023-09-29 17:56:42 -0400

    Add responsive formatting

--- src/components/Informational.jsx
+++ src/components/Informational.jsx
@@ -7,7 +7,7 @@ function Informational(props) {
     );
 
     return(
-        <div className="container px-5 text-dark">
+        <div className="col-span-2 lg:col-span-1 container mt-4 px-lg-5 text-dark whitespace-pre-wrap">
                 <h2 className="pb-4 font-title text-center font-bold">
                     {props.title}
                 </h2>
