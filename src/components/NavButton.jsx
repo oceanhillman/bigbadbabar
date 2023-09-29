fba35b617f9446aa56812a4d36cbbb4931f463c5
Ocean Hillman <oceanhillman@gmail.com>
2023-09-29 14:08:12 -0400

    Fix breakpoint not working

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function NavButton(props) {
     return (
-        <NavLink to={props.url} className="mx-1 lg:mx-9 font-nav font-extrabold no-underline text-xl p-2 text-dark hover:text-almost-white">
+        <NavLink to={props.url} className="sm:mx-1 lg:mx-8 font-nav font-extrabold no-underline text-xl p-2 text-dark hover:text-almost-white">
             {props.name}
         </NavLink>
     )
