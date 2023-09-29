29e41729fd468895de3ee2c5b0bef5e914b6425f
Ocean Hillman <oceanhillman@gmail.com>
2023-09-29 15:34:40 -0400

    Adjust margin

--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -2,7 +2,7 @@
 function YoutubeEmbed(props) {
     return (
             <iframe
-                className="mb-5 shadow w-full h-[250px] sm:h-[300px] lg:h-[320px]"
+                className="mb-4 shadow w-full h-[250px] sm:h-[300px] lg:h-[320px]"
                 src={`https://www.youtube.com/embed/${props.embedId}`}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
