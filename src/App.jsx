1d740966838afd2f91b3afb139733f4238e2f21b
Ocean Hillman <oceanhillman@gmail.com>
2023-10-05 16:16:31 -0400

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import OnlinePoker from "./pages/OnlinePoker";
 import Contact from "./pages/Contact";
 import GoodStuff from "./pages/GoodStuff";
+import IgnitionCasino from "./pages/IgnitionCasino";
+import AmericasCardroom from "./pages/AmericasCardroom";
+import SwcPoker from "./pages/SwcPoker";
+import PokerBros from "./pages/PokerBros";
+import Pokerrrr2 from "./pages/Pokerrrr2";
+import KingsClubPoker from "./pages/KingsClubPoker";
+import InternationalPoker from "./pages/InternationalPoker";
 
 function App() {
   return (
@@ -9,9 +17,16 @@ function App() {
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
+          <Route path="online-poker" element={<OnlinePoker />} />
+          <Route path="ignition-casino" element={<IgnitionCasino />} />
+          <Route path="americas-cardroom" element={<AmericasCardroom />} />
+          <Route path="swc-poker" element={<SwcPoker />} />
+          <Route path="poker-bros" element={<PokerBros />} />
+          <Route path="pokerrrr-2" element={<Pokerrrr2 />} />
+          <Route path="kings-club-poker" element={<KingsClubPoker />} />
+          <Route path="international-poker" element={<InternationalPoker />} />
           <Route path="contact" element={<Contact />} />
           <Route path="good-stuff" element={<GoodStuff />} />
-          {/*<Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
