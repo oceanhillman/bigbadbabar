f49dcfd1122faa7ea4d270b3f88de65942fd4b76
Ocean Hillman <oceanhillman@gmail.com>
2023-10-05 16:19:04 -0400

    Add dynamic column span

--- src/components/Informational.jsx
+++ src/components/Informational.jsx
@@ -1,17 +1,17 @@
 
 function Informational(props) {
     const body = props.content?.map((paragraph, index) => 
-        <p className="font-body text-xl" key={index}>
+        <p className="font-body text-xl " key={index}>
             {paragraph}
         </p>
     );
 
     return(
-        <div className="col-span-2 lg:col-span-1 container mt-4 px-lg-5 text-dark whitespace-pre-wrap">
-                <h2 className="pb-4 font-title text-center font-bold">
-                    {props.title}
-                </h2>
-                {body}
+        <div className={`${"col-span-" + props.maxColumns} lg:col-span-1 mx-4 text-dark whitespace-pre-wrap`}>
+            <h2 className="pb-4 font-title text-center font-bold">
+                {props.title}
+            </h2>
+            {body}
         </div>
     );
 }
