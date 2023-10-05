914e98c6c8f2a38ebfacb012b528438cf7d7ecb9
Ocean Hillman <oceanhillman@gmail.com>
2023-10-05 16:17:13 -0400

    Fix button url not dynamic

--- src/components/ActionBanner.jsx
+++ src/components/ActionBanner.jsx
@@ -8,13 +8,13 @@ function ActionBanner(props) {
     );
 
     return(
-        <div className="mt-2 py-5 text-almost-white bg-dark/30 lg:bg-transparent lg:bg-gradient-to-r from-dark/10 from-10% via-dark/30 to-dark/10 to-90%">
+        <div className="w-full mt-2 py-5 text-almost-white bg-dark/30 lg:bg-transparent lg:bg-gradient-to-r from-dark/10 from-10% via-dark/30 to-dark/10 to-90%">
             <div className="text-center">
                 <h1 className="pb-3 font-title font-bold">
                     {props.title}
                 </h1>
                 {body}
-                <Link to="/contact">
+                <Link to={props.actionURL}>
                     <button className="mt-4 bg-almost-white hover:bg-gray-200 text-dark font-semibold py-2 px-10 border border-dark rounded-full drop-shadow-md shadow-dark">
                         {props.call}
                     </button>
