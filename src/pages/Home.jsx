c519f4523d4b38d235de8d43a01e8f92e8e939a6
Ocean Hillman <oceanhillman@gmail.com>
2023-10-05 16:21:30 -0400

    Update component props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import customer from "../images/customer.svg";
 
 function Home() {
     return (
-        <React.Fragment>
+        <div className="flex flex-col min-h-screen max-w-screen">
             <Header />
             <ActionBanner 
                 title={"Welcome!"}
@@ -18,28 +18,33 @@ function Home() {
                     "You've come to the right place for info and resources. I've been at this since 2006 and I know what's up.",
                     "We've got you covered with classic sites and new offerings.",
                 ]}
-                call="Contact me!"
-                actionURL="/contact"
+                call="Get started!"
+                actionURL="/online-poker"
             />
-            <div className="grid grid-cols-2 auto-cols-max xl:mx-28 lg:mt-24 pb-5 lg:divide-x divide-dark">
-                <Informational
-                    title={"What's Rakeback?"}
-                    content={[
-                        "In any poker room - live or online - the house keeps a share of each pot.  This is the rake, and is the house's way of getting paid.",
-                        "In brick-and-mortar casinos, this money is gone (except for a promo or lucky jackpot).",
-                        "In online poker, however, the sites give some of this money back to you, as a reward and an incentive. This rakeback can have a big impact on your bottom line.",
-                        "While it isn't the only factor you should weigh when choosing a site, rakeback is important. Different sites give back different percentages, and they have different bonuses and promos that can boost your bankroll. Let's compare everything and find the best spot for you."
-                    ]}
-                />
-                <Informational
-                    title={"What's an Affiliate?"}
-                    content={[
-                        "Online poker, like many other industries, uses affiliate marketing. Affiliates (like me) refer new players to a poker site, saving the site money on advertising. The player plays on the site, and the site profits and gives some back to the affiliate.",
-                        "What many people don't realize is how important a good affiliate is. The right person in your corner can help you have a smoother, more profitable experience across the board.",
-                        "I want to see you succeed, and I'll help you can make the best decisions. This includes great information and advice now, and support with anything that might happen in the future. Most of my clients work with me for many years and end up referring their friends.",
-                        "I've been around for a while and my reputation is important to me. Let's work together!"
-                    ]}
-                />
+            <div className="grid grid-cols-2 auto-cols-max mt-5 xl:mx-28 lg:divide-x divide-dark">
+                <div className="col-span-2 lg:col-span-1 px-lg-5">
+                    <Informational
+                        title={"What's Rakeback?"}
+                        content={[
+                            "In any poker room - live or online - the house keeps a share of each pot.  This is the rake, and is the house's way of getting paid.",
+                            "In brick-and-mortar casinos, this money is gone (except for a promo or lucky jackpot).",
+                            "In online poker, however, the sites give some of this money back to you, as a reward and an incentive. This rakeback can have a big impact on your bottom line.",
+                            "While it isn't the only factor you should weigh when choosing a site, rakeback is important. Different sites give back different percentages, and they have different bonuses and promos that can boost your bankroll. Let's compare everything and find the best spot for you."
+                        ]}
+                        maxColumns={2}
+                    />
+                </div>
+                <div className="col-span-2 lg:col-span-1 px-lg-5">
+                    <Informational
+                        title={"What's an Affiliate?"}
+                        content={[
+                            "Online poker, like many other industries, uses affiliate marketing. Affiliates (like me) refer new players to a poker site, saving the site money on advertising. The player plays on the site, and the site profits and gives some back to the affiliate.",
+                            "What many people don't realize is how important a good affiliate is. The right person in your corner can help you have a smoother, more profitable experience across the board.",
+                            "I want to see you succeed, and I'll help you can make the best decisions. This includes great information and advice now, and support with anything that might happen in the future. Most of my clients work with me for many years and end up referring their friends.",
+                            "I've been around for a while and my reputation is important to me. Let's work together!"
+                        ]}
+                    />
+                </div>
             </div>
             <div className="flex flex-col justify-content-center items-center">
                 <Testimonial />
@@ -49,14 +54,14 @@ function Home() {
                 content={[
                     "Click the button below to get in contact."
                 ]}
-                call="Get Started Now"
-                action="/contact"
+                call="Contact me"
+                actionURL="/contact"
             />
             <br></br>
             <br></br>
             <br></br>
             <Footer />
-        </React.Fragment>
+        </div>
         
     );
 }
