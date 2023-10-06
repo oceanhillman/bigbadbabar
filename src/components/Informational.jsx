7760af798be5f346536829ebfa3d27db4c7cf71e
Ocean Hillman <oceanhillman@gmail.com>
2023-10-06 14:16:07 -0400

    Remove margin

--- src/components/Informational.jsx
+++ src/components/Informational.jsx
@@ -1,14 +1,14 @@
 
 function Informational(props) {
     const body = props.content?.map((paragraph, index) => 
-        <p className="font-body text-xl " key={index}>
+        <p className="font-body text-xl" key={index}>
             {paragraph}
         </p>
     );
 
     return(
         <div className={`${"col-span-" + props.maxColumns} lg:col-span-1 mx-4 text-dark whitespace-pre-wrap`}>
-            <h2 className="pb-4 font-title text-center font-bold">
+            <h2 className="m-0 pb-4 font-title text-center font-bold">
                 {props.title}
             </h2>
             {body}
