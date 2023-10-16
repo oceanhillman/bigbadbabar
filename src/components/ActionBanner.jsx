06de6a292fb82444288ece2eeacf6fcac49bfaba
Ocean Hillman <oceanhillman@gmail.com>
2023-10-16 16:18:55 -0400

    Adjust style and formatting

--- src/components/ActionBanner.jsx
+++ src/components/ActionBanner.jsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 
 function ActionBanner(props) {
-    const body = props.content?.map((paragraph, index) => 
-        <p className="font-body text-xl" key={index}>
-            {paragraph}
-        </p>
-    );
-
+    
     return(
-        <div className="w-full mt-2 py-5 text-almost-white bg-dark/30 lg:bg-transparent lg:bg-gradient-to-r from-dark/10 from-10% via-dark/30 to-dark/10 to-90%">
+        <div className="mt-2 mx-3 py-4 text-dark">
             <div className="text-center">
-                <h1 className="pb-3 font-title font-bold">
+                <h1 className="font-serif">
                     {props.title}
                 </h1>
-                {body}
-                <Link to={props.actionURL}>
-                    <button className="mt-4 bg-almost-white hover:bg-gray-200 text-dark font-semibold py-2 px-10 border border-dark rounded-full drop-shadow-md shadow-dark">
-                        {props.call}
+
+                {props.bodyText?.map((paragraph, index) => 
+                    <p className="my-[10px] font-sans text-xl" key={index}>
+                        {paragraph}
+                    </p>
+                )}
+                
+                <Link to={props.buttonUrl}>
+                    <button className="mt-2 bg-teal-blue hover:bg-moonstone-blue text-almost-white font-semibold py-2 px-10 rounded-3xl">
+                        {props.buttonText}
                     </button>
                 </Link>
             </div>
