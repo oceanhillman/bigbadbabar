f38e14f8b345a3533fe7fa0771f6c8d4d80b52b9
Ocean Hillman <oceanhillman@gmail.com>
2023-10-16 16:21:19 -0400

    Move title outside of form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,10 @@ function Contact() {
         <div className="flex flex-col min-h-screen">
             <Header />
             <div className="flex-1">
-                <div className="flex mx-auto sm:my-2 md:my-3 lg:my-4 xl:my-5 max-w-xl">
+                <div className="flex flex-col mx-auto sm:my-2 md:my-3 lg:my-4 xl:my-5 max-w-xl">
+                    <h1 className="text-center font-serif text-dark">
+                        Contact Me
+                    </h1>
                     <ContactForm />
                 </div>
             </div>
