940ce24b385899de3ee7ab9f4c027d8f19027eeb
Ocean Hillman <oceanhillman@gmail.com>
2023-10-19 17:34:21 -0400

    Add containers to embeds and adjust text elements

--- src/pages/GoodStuff.jsx
+++ src/pages/GoodStuff.jsx
@@ -11,37 +11,47 @@ function GoodStuff() {
         <div className="flex flex-col min-h-screen">
             <Header />
             <div className="flex-1">
-                <div className="flex flex-col items-center mx-auto mt-4 max-w-xl">
-                    <h1 className="text-center font-serif text-dark">
+                <div className="flex flex-col items-center mx-auto max-w-xl">
+                    <h1 className="my-4 text-center font-serif text-dark">
                         Behold - Cool things!
                     </h1>
-                    <div className="flex flex-row items-center mt-4 mb-2">
+                    <div className="flex flex-row justify-start items-center mb-2 mx-2">
                         <SocialButton
                             icon={bloggerIcon}
                             url="http://jessetakesashot.blogspot.com/"
                             type="Blogger"
                             size={16}
                         />
-                        <Link to={"http://jessetakesashot.blogspot.com"} target="_blank" rel='noopener noreferrer' className="-ml-6 text-lg text-dark font-body block">
+                        <Link to={"http://jessetakesashot.blogspot.com"} target="_blank" rel='noopener noreferrer' className="-ml-6 text-dark font-sans text-xl block">
                             My friend's blog - the life and times of a SoCal pro.
                         </Link>
                     </div>
-                    <h5 className="font-body mt-4 mb-1 text-center">
+                    <p className="font-sans mt-4 mb-1 text-center text-xl">
                         Blergh! - my favorite seal video - you won't be disappointed.
-                    </h5>
-                    <YoutubeEmbed embedId="3L7VJl76i9U" />
-                    <h5 className="font-body mt-4 mb-1 text-center">
+                    </p>
+                    <div className="w-full h-[250px] sm:h-[300px] lg:h-[320px] rounded-3xl overflow-hidden">
+                        <YoutubeEmbed embedId="3L7VJl76i9U" />
+                    </div>
+                    <p className="font-sans mt-4 mb-1 text-center text-xl">
                         Second favorite! - cute baby seal gets rescued.
-                    </h5>
-                    <YoutubeEmbed embedId="dnqV1qAwpf0" />
-                    <h5 className="font-body mt-4 mb-1 text-center">
+                    </p>
+                    <div className="w-full h-[250px] sm:h-[300px] lg:h-[320px] rounded-3xl overflow-hidden">
+                        <YoutubeEmbed embedId="dnqV1qAwpf0" />
+                    </div>
+                    <p className="font-sans mt-4 mb-1 text-center text-xl">
                         And a third! - okay look I just really like seals.
-                    </h5>
-                    <YoutubeEmbed embedId="C9_UZ-8Uc2s" />
-                    <h5 className="font-body mt-4 mb-1 text-center">
+                    </p>
+                    <div className="w-full h-[250px] sm:h-[300px] lg:h-[320px] rounded-3xl overflow-hidden">
+                        <YoutubeEmbed embedId="C9_UZ-8Uc2s" />
+                    </div>
+                    <p className="font-sans mt-4 mb-1 text-center text-xl">
                         Stompy Siamese - cats are cool too.
-                    </h5>
-                    <YoutubeEmbed embedId="LmKx-Ut5MpU" />
+                    </p>
+                    <div className="w-full h-[250px] sm:h-[300px] lg:h-[320px] rounded-3xl overflow-hidden">
+                        <YoutubeEmbed embedId="LmKx-Ut5MpU" />
+                    </div>
+                    <br></br>
+                    <br></br>
                 </div>
             </div>
             <Footer />
