88a5fcb62cad232bc3f9f69a0e3dc01e65bb454f
Ocean Hillman <oceanhillman@gmail.com>
2023-10-19 17:32:24 -0400

    Create component

---
+++ src/components/MobileNavMenu.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import Brand from "./Brand";
+import MobileNavButton from "./MobileNavButton";
+import MobileDropdown from "./MobileDropdown";
+import { AiOutlineClose } from "react-icons/ai";
+import hamburgerMenuIcon from "../images/hamburger-menu.svg";
+
+function MobileNavMenu() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function HamburgerMenu() {
+
+        const handleClick = () => setIsOpen(!isOpen);
+
+        return (<img src={hamburgerMenuIcon} className="justify-self-start ml-4 mr-auto " onClick={handleClick}/>);
+    }
+
+    function closeMenu() {
+        setIsOpen(false);
+    }
+    
+    return (
+        <div className="tablet:hidden">
+            <div className="grid grid-cols-5 align-items-center justify-center text-center py-2">
+                <HamburgerMenu />
+                <Brand />
+            </div>
+            <nav className={`z-1 fixed bottom-0 left-0 w-screen flex flex-col justify-between ${isOpen ? "h-screen " : "h-0 "} overflow-hidden transition-all delay-150 duration-300 bg-teal-blue`}>
+                <div className={`m-2 p-2 self-end`} onClick={closeMenu}>
+                    <AiOutlineClose className="text-almost-white text-4xl" />
+                </div>
+                <div className="mb-[200px]">
+                    <MobileNavButton 
+                        title="Home"
+                        url="/"
+                    />
+                    <MobileDropdown 
+                        title="Poker"
+                        items={[
+                            {title:"Play Online Poker", url:"/online-poker"},
+                            {title:"Ignition Casino", url:"/ignition-casino"},
+                            {title:"America's Cardroom", url:"/americas-cardroom"},
+                            {title:"SwC Poker", url:"/swc-poker"},
+                            {title:"Poker Bros", url:"/poker-bros"},
+                            {title:"Pokerrrr 2", url:"/pokerrrr-2"},
+                            {title:"Kings Club Poker", url:"/kings-club-poker"},
+                            {title:"International Poker", url:"/international"},
+                        ]}
+                    />
+                    <MobileNavButton 
+                        title="Contact"
+                        url="/contact"
+                    />
+                    <MobileNavButton 
+                        title="Good Stuff"
+                        url="/good-stuff"
+                    />
+                </div>
+                <div></div>
+            </nav>
+        </div>
+    )
+}
+
+export default MobileNavMenu;
