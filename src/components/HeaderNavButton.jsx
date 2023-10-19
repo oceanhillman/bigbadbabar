121a819dacf1843fc5fab093394e026263abe380
Ocean Hillman <oceanhillman@gmail.com>
2023-10-19 17:30:32 -0400

    Adjust responsiveness

--- src/components/HeaderNavButton.jsx
+++ src/components/HeaderNavButton.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function HeaderNavButton(props) {
     return (
-        <NavLink to={props.path} className="tablet:mx-1 desktop:mx-8 p-2 text-2xl text-dark no-underline font-nav">
+        <NavLink to={props.path} className="laptop:w-[160px] mx-2 p-2 text-2xl text-dark hover:text-teal-blue whitespace-nowrap no-underline font-nav">
             {props.name}
         </NavLink>
     )
