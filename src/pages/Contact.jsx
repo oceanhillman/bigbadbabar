2bb113bd57326db4c25835d2a69103cb8b50e668
Ocean Hillman <oceanhillman@gmail.com>
2023-10-19 17:34:00 -0400

    Adjust top margin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ function Contact() {
             <Header />
             <div className="flex-1">
                 <div className="flex flex-col mx-auto sm:my-2 md:my-3 lg:my-4 xl:my-5 max-w-xl">
-                    <h1 className="text-center font-serif text-dark">
+                    <h1 className="my-4 text-center font-serif text-dark">
                         Contact Me
                     </h1>
                     <ContactForm />
