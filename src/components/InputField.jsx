88481a11734cb646f076d83ce2874ef6cbe4dbb5
Ocean Hillman <oceanhillman@gmail.com>
2023-10-24 12:20:13 -0400

    Fix style disparity on mobile browsers

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -5,7 +5,7 @@ function InputField(props) {
             name={props.name}
             type={props.type} 
             placeholder={props.placeholder + (props.required ? "*" : "")}
-            className={`h-10 my-2 pt-3 bg-transparent border-b border-dark text-dark font-sans focus:outline-none 
+            className={`h-10 my-2 pt-3 bg-transparent border-b border-dark text-dark font-sans focus:outline-none rounded-none
                 ${props.size==="small" ? "col-span-1" : "col-span-2"} ${props.highlight === true ? "placeholder-red-400" : "placeholder-dark"}`}
             onChange={props.onChange}
         />
